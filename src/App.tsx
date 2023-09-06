021cfbc85603d704bc6b05558315fba362acd3ed
roehilldev <roehilldev@gmail.com>
2023-09-06 13:10:12 +0200

    Single pages for product and user

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Navbar from "./components/navbar/Navbar";
 import { Login } from "./pages/login/Login";
 import "./styles/global.scss";
 import Footer from "./components/footer/Footer";
+import User from "./pages/user/User";
+import Product from "./pages/product/Product";
 
 function App() {
   const Layout = () => {
@@ -40,8 +42,16 @@ function App() {
           element: <Users />,
         },
         {
+          path: "/users/:id",
+          element: <User />,
+        },
+        {
           path: "/products",
           element: <Products />,
+        },
+        {
+          path: "/products/:id",
+          element: <Product />,
         },
       ],
     },
